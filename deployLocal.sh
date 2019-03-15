d9f66614110903db8e647b36d320cfce93eb8601
pcb <per.christian.bjelke@bibsys.no>
2019-03-15 13:25:43 +0100

    host

--- deployLocal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo Deploying to docker
-docker-compose down || true && \
-docker-compose rm -f || true && \
-docker-compose --compatibility up -d --build
-
-date
--- unDeployLocal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo Undeploying from docker
-docker-compose down || true && \
-docker-compose rm -f || true &&
-
-date
